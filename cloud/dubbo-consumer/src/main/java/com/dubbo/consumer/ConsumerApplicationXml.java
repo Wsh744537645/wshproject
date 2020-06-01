2d7b96dc85a9121fbd9611a5514dafab88666aea
wshstephen <wushuihua@jmfen.cn>
2020-06-01 16:13:11 +0800

    修改dubbo demo启动类命名

--- cloud/dubbo-consumer/src/main/java/com/dubbo/consumer/ConsumerApplicationXml.java
+++ cloud/dubbo-consumer/src/main/java/com/dubbo/consumer/ConsumerApplicationXml.java
@@ -6,10 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.io.IOException;
 
 /**
+ * 参照资料：https://segmentfault.com/a/1190000019896723
  * @author jmfen
  * date 2020-06-01
  */
-public class ConsumerApplication {
+public class ConsumerApplicationXml {
 
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
--- cloud/dubbo-provider/src/main/java/com/dubbo/provider/ApplicationXml.java
+++ cloud/dubbo-provider/src/main/java/com/dubbo/provider/ApplicationXml.java
@@ -5,10 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.io.IOException;
 
 /**
+ * 参照资料：https://segmentfault.com/a/1190000019896723
  * @author jmfen
  * date 2020-05-30
  */
-public class Application {
+public class ApplicationXml {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("METE-INF/spring/provider.xml");
         context.start();
